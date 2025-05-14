5b960e871b9fec8db518375bbeda337d8a62f718
Sodiq Farhan A. <sodiqfarhan360@gmail.com>
2025-05-14 18:33:58 +0100

    Complete chat feature

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const port = process.env.PORT || 3000;
+
+  // Serve static files from the 'public' directory
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+
+  // Allow CORS for all origins
+  app.enableCors({
+    origin: '*',
+  });
+
   await app.listen(port);
-  console.log(`Application is running on: ${port}`);
+  console.log(`Application is running on port: ${port}`);
 }
 bootstrap();
